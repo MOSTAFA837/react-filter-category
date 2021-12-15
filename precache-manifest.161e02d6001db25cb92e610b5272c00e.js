a5848890d40f1447a3adbbb1546fe9eb0c559b42
MOSTAFA837 <mostafamanchester837@gmail.com>
2021-12-15 13:17:16 +0200

    Updates

---
+++ precache-manifest.161e02d6001db25cb92e610b5272c00e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7294bb627bda318de943f8251377951",
+    "url": "/react-filter-category/index.html"
+  },
+  {
+    "revision": "756fdb2131a3baea7588",
+    "url": "/react-filter-category/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/react-filter-category/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-filter-category/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756fdb2131a3baea7588",
+    "url": "/react-filter-category/static/js/main.5ddb5036.chunk.js"
+  },
+  {
+    "revision": "bb3ada0302adb90f8fbb",
+    "url": "/react-filter-category/static/js/runtime-main.60799500.js"
+  }
+]);
